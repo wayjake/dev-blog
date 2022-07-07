ba2e9bb6fda72bb9bc9f2a209708f744a3fa8209
Daniel DiVenere <danieldivenere@gmail.com>
2022-07-07 10:58:19 -0700

    filter out unpublished posts

--- src/api/Database/getDatabasePosts.ts
+++ src/api/Database/getDatabasePosts.ts
@@ -2,10 +2,21 @@ import { notion } from "../utils/notion";
 
 // Pull of Database Object/List will return a list of objects (pages)
 export const getDatabasePosts = async () => {
-    const response = await notion.databases.query({
-      //@ts-ignore
-      //database id string is used here now
-      database_id: `${process.env.NOTION_DATABASE_ID}`,
-    });
-    return response.results;
-  };
+  const response = await notion.databases.query({
+    filter: {
+      and: [
+        {
+          "property": "Published",
+          "checkbox": {
+            "equals": true
+          }
+        }
+      ]
+    },
+    //@ts-ignore
+    //database id string is used here now
+    database_id: `${process.env.NOTION_DATABASE_ID}`,
+  });
+
+  return response.results;
+};
